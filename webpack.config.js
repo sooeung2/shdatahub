f241cd6e9f0f01a4b5d13bb4937a57f86b0efe62
Edward Guo <edward.h.guo@gmail.com>
2017-01-14 11:18:51 -0800

    initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: '.src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['es2015', 'react'] } },
+      { test: /\.s?css$/, loaders: ['style', 'css', 'sass'] },
+      { test: /\.png$|\.jpe?g$/, loader: 'file' }
+    ]
+  },
+  resolve: {
+    modulesDirectories: ['src', 'node_modules'],
+    extensions: ['', '.json', '.js', '.jsx']
+  },
+  stats: { colors: true },
+  watch: true,
+  progress: true,
+  devServer: {
+    contentBase: './build',
+    inline: true,
+    historyApiFallback: true
+  }
+}
